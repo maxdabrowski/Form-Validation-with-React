3b3e245c037aaf85f5f9f1d63bef2a578ad3b883
Max <maksymilian.dabrowski93@gmail.com>
2019-04-01 00:02:29 +0200

    Updates

---
+++ precache-manifest.1803a92d40c92b799a7d02df3b9eec56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7145273dca4adc8228b8",
+    "url": "/Form-Validation-with-React/static/css/main.07344419.chunk.css"
+  },
+  {
+    "revision": "7145273dca4adc8228b8",
+    "url": "/Form-Validation-with-React/static/js/main.7145273d.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/Form-Validation-with-React/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "4e8efc768eca452f7520",
+    "url": "/Form-Validation-with-React/static/js/runtime~main.4e8efc76.js"
+  },
+  {
+    "revision": "29e60a539d40ad944270661bebef4f74",
+    "url": "/Form-Validation-with-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Form-Validation-with-React-/precache-manifest.b2f87bb40b2379fde7f4f338ef2af7fd.js"
+  "/Form-Validation-with-React/precache-manifest.1803a92d40c92b799a7d02df3b9eec56.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Form-Validation-with-React-/index.html", {
+workbox.routing.registerNavigationRoute("/Form-Validation-with-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
